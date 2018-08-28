3b2dde05635a0fcc73f75d9a0e0355040f3a296f
Jed Peek <jedpeek@yahoo.com>
2018-08-28 21:48:26 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,7 @@ Rails.application.routes.draw do
    post '/login' => 'sessions#create'
    post '/logout' => 'sessions#destroy'
    get '/secret' => 'secrets#show'
-
-   post '/login' => 'sessions#create'
-   get '/secret' => 'secrets#show'
    get '/welcome' => 'sessions#welcome'
-   get '/' => 'sessions#new'
   #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
