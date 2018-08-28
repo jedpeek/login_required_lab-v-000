f5502cc22df4934c9b2fdec4818019d15dd6fa29
Jed Peek <jedpeek@yahoo.com>
2018-08-28 21:47:03 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
    root 'sessions#new'
-   post '/' => 'sessions#create'
+   post '/create' => 'sessions#create'
    get '/secrets' => 'secrets#show'
    get '/welcome' => 'sessions#welcome'
    get '/' => 'sessions#new'
