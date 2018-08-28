7f3cd3862122e6d9f462615743ca11992beafb10
Jed Peek <jedpeek@yahoo.com>
2018-08-28 21:51:49 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
   end
 
   def create
+    raise params.inspect
     if params[:name]
       current_user = params[:name]
       redirect_to 'sessions#welcome'
